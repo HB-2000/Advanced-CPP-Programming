9131cabe7ef973c6b45a8da7f399d8d007a5004e
HB <hussein.bazmandegan@gmail.com>
2025-04-25 19:36:19 +0330

    change CPP_01_01.cpp

--- Codes/CPP_01/CPP_01_01.cpp
+++ Codes/CPP_01/CPP_01_01.cpp
@@ -2,49 +2,34 @@
 #include <string>
 using namespace std;
 
-// تابع برای دریافت اطلاعات یک دانشجو
-void getStudentInfo(string &name, int &age, float &gpa) {
-    cout << "Enter student name: ";
-    getline(cin, name);
-    cout << "Enter student age: ";
-    cin >> age;
-    cout << "Enter student GPA: ";
-    cin >> gpa;
-    cin.ignore(); // برای پاک کردن بافر ورودی
-}
-
-// تابع برای چاپ اطلاعات دانشجو
-void printStudentInfo(string name, int age, float gpa) {
-    cout << "Student with higher GPA:" << endl;
-    cout << "Name: " << name << endl;
-    cout << "Age: " << age << endl;
-    cout << "GPA: " << gpa << endl;
-}
-
 int main() {
-    // متغیرهای دانشجوی اول
+
     string name1;
     int age1;
     float gpa1;
 
-    // متغیرهای دانشجوی دوم
     string name2;
     int age2;
     float gpa2;
 
-    // دریافت اطلاعات دانشجوی اول
-    cout << "Enter information for first student:" << endl;
-    getStudentInfo(name1, age1, gpa1);
+    cin >> name1;
+    cin >> age1;
+    cin >> gpa1;
 
-    // دریافت اطلاعات دانشجوی دوم
-    cout << "\nEnter information for second student:" << endl;
-    getStudentInfo(name2, age2, gpa2);
+    cin >> name2;
+    cin >> age2;
+    cin >> gpa2;
 
-    // مقایسه معدل‌ها و چاپ اطلاعات دانشجوی با معدل بالاتر
     if (gpa1 > gpa2) {
-        printStudentInfo(name1, age1, gpa1);
+        cout << "Student with higher GPA:" << endl;
+        cout << "Name: " << name1 << endl;
+        cout << "Age: " << age1 << endl;
+        cout << "GPA: " << gpa1 << endl;
     } else {
-        printStudentInfo(name2, age2, gpa2);
+        cout << "Student with higher GPA:" << endl;
+        cout << "Name: " << name2 << endl;
+        cout << "Age: " << age2 << endl;
+        cout << "GPA: " << gpa2 << endl;
     }
 
     return 0;
